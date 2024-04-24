128d0d8af97e819d51b5a79d3a1b5e663cd6d3ee
anniet0422 <160541416+anniet0422@users.noreply.github.com>
2024-04-24 14:55:40 -0500

    Add files via upload

---
+++ Login_ScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_ScreenApp.swift
+//  Login Screen
+//
+//  Created by Annie Tran on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Login_ScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
